fc1da56801de73019b7c5e1dab384513b1ffab64
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2022-10-18 11:10:54 +0200

    :sparkles: feat: sync openapi specs from specs repository

--- api/petstore/gen.go
+++ api/petstore/gen.go
@@ -1,4 +1,4 @@
 package petstore
 
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=types.cfg.yaml ../../openapi/petstore-expanded.yaml
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=server.cfg.yaml ../../openapi/petstore-expanded.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=types.cfg.yaml ../../openapi/petstore/spec.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=server.cfg.yaml ../../openapi/petstore/spec.yaml
